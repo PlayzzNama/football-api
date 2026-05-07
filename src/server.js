ec430950e96b4bb3cd47fb20c5a43cbc3b0c5c3e
Ivan/Awen <69popovichivan69@gmail.com>
2026-05-07 17:20:37 +0300

    Separate Express app from server startup

--- src/server.js
+++ src/server.js
@@ -1,19 +1,9 @@
-const express = require('express');
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config();
 
-const app = express();
 const port = process.env.PORT || 3000;
-
-app.use(express.json());
-
-app.get('/health', (req, res) => {
-  res.status(200).json({
-    status: 'ok',
-    service: 'football-api',
-  });
-});
 
 app.listen(port, () => {
   console.log(`Football API is running on port ${port}`);
